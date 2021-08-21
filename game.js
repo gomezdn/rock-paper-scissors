1430eb03f32575eec39ad3b7e6094c1a649eabb6
gomezdn <gomezdn@outlook.com>
2021-08-20 21:35:02 -0300

    Function added to play a round
    Function added to play a round against the computer.

--- game.js
+++ game.js
@@ -6,4 +6,34 @@ function computerPlay() {
     return (["Rock", "Paper", "Scissors"][randomNumberInRange(0, 2)])
 }
 
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return ("It's a draw! You both chose the same element.")
+    }
+        else if (winnerElement(playerSelection, computerSelection).toLowerCase() === playerSelection.toLowerCase()) {
+        return (`You won! ${playerSelection} beats ${computerSelection}`)
+    }
+        else if (winnerElement(playerSelection, computerSelection).toLowerCase() === computerSelection.toLowerCase()) {
+        return (`You lose :( ${computerSelection} beats ${playerSelection}`)
+    }
+}
+
+function winnerElement(element1, element2) {
+    if (element1.toLowerCase() === element2.toLowerCase()) {
+        return (element1)
+    }    
+        else if ( (element1.toLowerCase() === "rock" &&
+            element2.toLowerCase() === "paper") || (element1.toLowerCase() === "paper" && element2.toLowerCase() === "rock" ) ) {
+            return ("Paper")
+    }
+        else if ( (element1.toLowerCase() === "paper" &&
+            element2.toLowerCase() === "scissors") || (element1.toLowerCase() === "scissors" && element2.toLowerCase() ==="paper") ) {
+            return ("Scissors")
+        }
+        else if ( (element1.toLowerCase() === "rock" &&
+            element2.toLowerCase() === "scissors") || (element1.toLowerCase() === "scissors" && element2.toLowerCase() === "rock") ) {
+            return ("Rock")
+        }
+}
+
+
