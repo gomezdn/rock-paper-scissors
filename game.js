444b8a713ca74e8ea62f22b28efca17a34e11dab
gomezdn <gomezdn@outlook.com>
2021-08-27 03:16:36 -0300

    created and styled basic interface

--- game.js
+++ game.js
@@ -6,22 +6,6 @@ function computerPlay() {
     return (["Rock", "Paper", "Scissors"][randomNumberInRange(0, 2)])
 }
 
-function rockPaperOrScissors(){
-    switch (prompt(`Please enter the number of your option:\n1 - Rock\n2 - Scissors\n3 - Paper`)) {
-        case "1":
-            return ("Rock")
-            break;
-        case "2":
-            return ("Paper")
-            break;
-        case "3":
-            return ("Scissors")
-            break;
-        default:
-            alert("Please enter a number between 1-3");
-            rockPaperOrScissors()
-    }
-}
 function playRound() {
     let playerChoice = rockPaperOrScissors()
     let comChoice = computerPlay()
@@ -52,28 +36,3 @@ function winnerElement(element1, element2) {
         }
 }
 
-function game() {
-    let userScore = 0
-    let comScore = 0
-    for (let i = 0; i < 5; i++) {
-        let currentRound = playRound();
-        if (currentRound.startsWith("You won")) {
-            userScore++
-        }
-            else if (currentRound.startsWith("You lose")) {
-                comScore++
-            }
-        console.log(currentRound)
-    }
-    if (userScore - comScore > 0) {
-        return ("You won the game!")
-    }
-        else if (comScore - userScore > 0) {
-            return ("You lose the game :(")
-        }
-        else {
-            return ("It's a draw!")
-        }
-}
-
-console.log(game())
